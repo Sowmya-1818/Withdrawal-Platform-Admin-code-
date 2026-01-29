481abedf3341f320febb0c258f3216b81e685723
sowmya vintha <sowmyav@STRING.METAVERSE>
2026-01-29 12:02:19 +0530

    Remove ignored files from tracking

--- src/ApiConfig/Constants.js
+++
@@ -1,8 +0,0 @@
-// const API_SERVER_URL = "https://stradminapi.stringgames.io";
-const API_SERVER_URL = "https://stradminapi.stringgames.io";
-// const API_SERVER_URL = "https://apiadmin-telegames.strtesting.com";
-
-export const ENV_CONSTANTS = {
-  API_SERVER_URL,
- 
-};
